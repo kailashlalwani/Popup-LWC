7d1fdacd2df9ca5a55cd03660379862ee97bf9d4
kailashlalwani <58902509+kailashlalwani@users.noreply.github.com>
2021-09-24 10:51:50 +0530

    Update addMilestone.js

--- NewMilestone/force-app/main/default/lwc/addMilestone/addMilestone.js
+++ NewMilestone/force-app/main/default/lwc/addMilestone/addMilestone.js
@@ -1,7 +1,6 @@
-import { LightningElement, api , track } from 'lwc';
+import { LightningElement, track } from 'lwc';
 import { createRecord } from 'lightning/uiRecordApi';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
-//import saveFile from '@salesforce/apex/testcontroller.saveFile';
 import New_Milestone__c from '@salesforce/schema/New_Milestone__c';
 import Milestone_Name__c from '@salesforce/schema/New_Milestone__c.Milestone_Name__c';
 import Notified_to_When_Complete__c from '@salesforce/schema/New_Milestone__c.Notified_to_When_Complete__c'
@@ -9,16 +8,16 @@ import Description__c from '@salesforce/schema/New_Milestone__c.Description__c';
 
 export default class AddMilestone extends LightningElement 
 {
+
+    milestoneName = '';
+    notifyName = '';
+    descName = '';
+
     allowedFormats =  ['font', 'size', 'bold', 'italic', 'underline', 'strike',
     'list', 'indent', 'align', 'link', 'image', 'clean', 'table', 'header', 'color',
     'background','code','code-block'];
 
-    objectApiName = New_Milestone__c;
-    fields = [Milestone_Name__c, Notified_to_When_Complete__c, Description__c];
-
-    @api myRecordId;
     @track isModalOpen = false;
-    
     openModal() {
         // to open modal set isModalOpen tarck value as true
         this.isModalOpen = true;
@@ -27,31 +26,49 @@ export default class AddMilestone extends LightningElement
         // to close modal set isModalOpen tarck value as false
         this.isModalOpen = false;
     }
-    handleClick(event) {
-        this.clickedButtonLabel = event.target.label;
-        this.template.querySelector('lightning-record-edit-form').submit(this.fields);
-        this.isModalOpen = false;
-    }
-    handleSuccess(event) {
-        const toastEvent = new ShowToastEvent({
-            title: "New Milestone Created",
-            message: "Record ID: " + event.detail.id,
-            variant: "Success"
-        });
-        this.dispatchEvent(toastEvent);
-    }
-    saveAndNewClick() {
-        this.template.querySelector('lightning-record-edit-form').submit(this.fields);
-        const inputFields = this.template.querySelectorAll(
-            'lightning-input-field'
-        );
-        if (inputFields) {
-            inputFields.forEach(fields => {
-                fields.reset();
-            });
+    
+
+    mileChangeVal(event){
+        console.log(event.target.label);
+        console.log(event.target.value);        
+        if(event.target.label=='Milestone_Name__c'){
+            this.milestoneName = event.target.value;
         }
-       
-       
+        if(event.target.label=='Notified_to_When_Complete__c'){
+            this.notifyName = event.target.value;
+        }  
+                  
+    }
+
+    insertMilestoneAction(){
+        console.log(this.selectedMilestoneId);
+        const fields = {};
+        fields[Milestone_Name__c.fieldApiName] = this.milestoneName;
+        fields[Notified_to_When_Complete__c.fieldApiName] = this.notifyName;
+        fields[Description__c.fieldApiName] = this.descName;
+
+        const recordInput = { apiName: New_Milestone__c.objectApiName, fields };
+        createRecord(recordInput)
+        .then(milestoneobj =>{
+            this.mileId = milestoneobj.id;
+            this.dispatchEvent(
+                new ShowToastEvent({
+                    title: 'Success',
+                    message: 'New MileStone record has been created',
+                    variant: 'success',
+                }),
+            );
+        })
+
+        .catch(error => {
+            this.dispatchEvent(
+                new ShowToastEvent({
+                    title: 'Error creating record',
+                    message: error.body.message,
+                    variant: 'error',
+                }),
+            );
+        });
     }
     
     handleCancel(event){
